15466566b9a8337922bff69616d0672af539344b
Muhammad Mahad <muhammadmahad@Muhammads-MacBook-Pro-2.local>
2024-11-29 19:44:45 +0500

    added nishat and sapphire

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,19 @@ await Actor.init();
 
 const globs = {
   khaadi: ["https://pk.khaadi.com/**"],
-  sanaSafinaz: ["https://www.sanasafinaz.com/pk/*"],
+  sanaSafinaz: ["https://www.sanasafinaz.com/pk/**"],
   mariaB: [
-    "https://www.mariab.pk/*",
-    "https://www.mariab.pk/pages/*",
-    "https://www.mariab.pk/collections/*",
-    "https://www.mariab.pk/products/*",
+    "https://www.mariab.pk/**",
+    "https://www.mariab.pk/pages/**",
+    "https://www.mariab.pk/collections/**",
+    "https://www.mariab.pk/products/**",
   ],
-  gulAhmed: ["https://www.gulahmedshop.com/*"],
+  gulAhmed: ["https://www.gulahmedshop.com/**"],
+  nishatlinen: [
+    "https://nishatlinen.com/collections/**",
+    "https://nishatlinen.com/collections/*/products/**",
+  ],
+  sapphire: ["https://pk.sapphireonline.pk/collections/*/products/**"],
 };
 
 const exclusions = {
@@ -40,6 +45,42 @@ const exclusions = {
     "https://www.gulahmedshop.com/kids/*",
     "https://www.gulahmedshop.com/fragrances-perfumes/*",
     "https://www.gulahmedshop.com/fragrances-perfumes*",
+  ],
+  sapphire: [
+    "https://pk.sapphireonline.pk/collections/new-in-kids**",
+    "https://pk.sapphireonline.pk/collections/kids**",
+    "https://pk.sapphireonline.pk/collections/new-in-fragrances**",
+    "https://pk.sapphireonline.pk/collections/cosmetics**",
+    "https://pk.sapphireonline.pk/collections/fragrances**",
+    "https://pk.sapphireonline.pk/pages/shop-by-scent",
+    "https://pk.sapphireonline.pk/pages/the-edit",
+    "https://pk.sapphireonline.pk/collections/girls-eastern**",
+    "https://pk.sapphireonline.pk/collections/girls-kurtas**",
+    "https://pk.sapphireonline.pk/collections/girls-outfits**",
+    "https://pk.sapphireonline.pk/collections/girls-fusion**",
+    "https://pk.sapphireonline.pk/pages/mommy-and-me**",
+    "https://pk.sapphireonline.pk/collections/boys-eastern**",
+    "https://pk.sapphireonline.pk/collections/boys-kurta-shalwar**",
+    "https://pk.sapphireonline.pk/collections/boys-waistcoats**",
+    "https://pk.sapphireonline.pk/collections/boys-trousers**",
+    "https://pk.sapphireonline.pk/collections/boys-bedding**",
+    "https://pk.sapphireonline.pk/collections/girls-bedding**",
+    "https://pk.sapphireonline.pk/collections/cot-sets**",
+    "https://pk.sapphireonline.pk/collections/face**",
+    "https://pk.sapphireonline.pk/collections/lips**",
+    "https://pk.sapphireonline.pk/collections/eyes**",
+    "https://pk.sapphireonline.pk/collections/nails**",
+    "https://pk.sapphireonline.pk/collections/makeup-tools**",
+    "https://pk.sapphireonline.pk/collections/fragrance-rose-collection**",
+    "https://pk.sapphireonline.pk/collections/fragrance-discovery-sets**",
+    "https://pk.sapphireonline.pk/collections/womens-perfumes**",
+    "https://pk.sapphireonline.pk/collections/man-perfumes**",
+    "https://pk.sapphireonline.pk/collections/womens-body-mists**",
+    "https://pk.sapphireonline.pk/collections/men-s-body-mists**",
+    "https://pk.sapphireonline.pk/collections/floral-fragrances**",
+    "https://pk.sapphireonline.pk/collections/fruity-fragrances**",
+    "https://pk.sapphireonline.pk/collections/oriental-fragrances**",
+    "https://pk.sapphireonline.pk/collections/woody-fragrances**",
   ],
 };
 
@@ -212,6 +253,95 @@ const extractors = {
       return {};
     }
   },
+  "nishatlinen.com": async (page: Page) => {
+    // things to improve:
+    try {
+      // Retrieve the current page URL
+      const currentURL = page.url();
+
+      // Check if the URL contains the word 'products'
+      if (!currentURL.includes("products")) {
+        return {}; // Exit the function if 'products' is not in the URL
+      }
+
+      // Extract text from the 'h1' element
+      const h1Text = await page.textContent("h1");
+
+      // Extract text from all 'div' elements with the class 't4s-rte'
+      const divTexts = await page.$$eval("div.t4s-rte", (divs) =>
+        divs.map((div) => div.textContent?.trim()).join(" ")
+      );
+
+      // Combine the extracted texts into a single description
+      const description = `${h1Text?.trim()} ${divTexts}`.trim();
+
+      const imageUrls = await page.$$eval(
+        ".t4s-carousel__nav-item img",
+        (images) => {
+          const urls = images.map((img) => {
+            // Extract the highest-resolution URL from the srcset attribute
+            const srcset =
+              img.getAttribute("data-srcset") || img.getAttribute("srcset");
+            if (srcset) {
+              const srcsetUrls = srcset
+                .split(",")
+                .map((src) => src.trim().split(" ")[0]);
+              return "https:" + srcsetUrls[srcsetUrls.length - 1]; // Get the last URL, which is usually the highest resolution
+            }
+            // Fallback to the src attribute if srcset is not available
+            return img.getAttribute("src");
+          });
+          // Remove duplicates
+          return [...new Set(urls)];
+        }
+      );
+
+      return { description, imageUrls };
+    } catch (error) {
+      return {};
+    }
+  },
+  "pk.sapphireonline.pk": async (page: Page) => {
+    // things to improve:
+    try {
+      // Retrieve the current page URL
+      const currentURL = page.url();
+
+      // Check if the URL contains the word 'products'
+      if (!currentURL.includes("products")) {
+        return {}; // Exit the function if 'products' is not in the URL
+      }
+
+      const description = await page.evaluate(() => {
+        const container = document.querySelector("#content1");
+        if (!container) return ""; // Handle cases where the container is not found
+
+        return container.innerHTML
+          .split(/<br\s*\/?>/i) // Split by <br> tags
+          .map((text) => text.replace(/<\/?[^>]+(>|$)/g, "").trim()) // Remove HTML tags and trim whitespace
+          .filter((text) => text) // Remove empty strings
+          .join("\n"); // Preserve line breaks
+      });
+
+      // Extract URLs of the highest-resolution images
+      const imageUrls = await page.$$eval(
+        "div[data-product-single-media-wrapper] img", // Select the image elements within the specified container
+        (images) =>
+          images
+            .map(
+              (img) => "https:" + img.getAttribute("data-master") // Extract the `data-master` attribute for the highest resolution image
+            )
+            .filter(Boolean) // Remove null/undefined values
+      );
+      //   console.log("description", description);
+      //   console.log("imageUrls", imageUrls);
+
+      return { description, imageUrls };
+    } catch (error) {
+      console.error(error);
+      return {};
+    }
+  },
 
   // Define extractors for other domains
 };
@@ -256,12 +386,15 @@ const crawler = new PlaywrightCrawler({
         ...globs.sanaSafinaz,
         ...globs.mariaB,
         ...globs.gulAhmed,
+        ...globs.nishatlinen,
+        ...globs.sapphire,
       ],
       exclude: [
         ...exclusions.khaadi,
         ...exclusions.sanaSafinaz,
         ...exclusions.mariaB,
         ...exclusions.gulAhmed,
+        ...exclusions.sapphire,
       ],
       //   transformRequestFunction: (request) => {
       //     console.log(`Enqueuing URL: ${request.url}`);
@@ -278,12 +411,12 @@ const crawler = new PlaywrightCrawler({
 // List of initial URLs to crawl
 const startUrls = [
   //   "https://pk.khaadi.com/",
-  "https://www.sanasafinaz.com/pk/",
+  //   "https://www.sanasafinaz.com/pk/",
   //   "https://www.mariab.pk/",
   //   "https://www.gulahmedshop.com/",
-
   //   "https://nishatlinen.com/",
-  //   "https://pk.sapphireonline.pk/",
+
+  "https://pk.sapphireonline.pk/",
   //   "https://bareeze.com/",
   //   "https://www.junaidjamshed.com/",
   //   "https://asimjofa.com/",
